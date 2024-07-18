234dc2edeac6b319c25ea2ff82dd3c1622cf7867
Jeren Raquel <jeraquel101@gmail.com>
2024-07-17 21:31:16 -0700

    patches

--- battle_systems/States/PlayerState.gd
+++ battle_systems/States/PlayerState.gd
@@ -1,12 +1,15 @@
 extends Node
 
 @onready var initiative_tracker: BattlefieldInitiativeTracker = %InitiativeTracker
+@onready var control_shield: Panel = %ControlShield
 
 func enter() -> void:
 	# regen AP
+	control_shield.hide()
 	pass
 
 func exit() -> void:
+	control_shield.show()
 	initiative_tracker.report_requeue()
 
 func update(_delta: float) -> void:
