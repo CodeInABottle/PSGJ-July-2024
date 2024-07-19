562d56f96f066f31b79265455dd080a8fddc3ff9
Jeren Raquel <jeraquel101@gmail.com>
2024-07-19 16:23:22 -0700

    updated UI layout + flask indicators

--- battle_systems/Objects/FlaskBar/FlaskBar.gd
+++ battle_systems/Objects/FlaskBar/FlaskBar.gd
@@ -3,9 +3,22 @@ extends TextureProgressBar
 
 @export var color: Color
 
+@onready var label: Label = $Label
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _ready() -> void:
+	label.hide()
 	tint_progress = color
 	value = 100
+	label.add_theme_color_override("font_color", color)
 
 func update_value(current: int) -> void:
 	value = float(current)
+	label.text = str(value) + "/" + str(max_value)
+
+func _on_area_2d_mouse_entered() -> void:
+	animation_player.play("Sway")
+
+func _on_area_2d_mouse_exited() -> void:
+	animation_player.stop()
+	label.hide()
--- battle_systems/Objects/Table/TableManager.gd
+++ battle_systems/Objects/Table/TableManager.gd
@@ -14,15 +14,17 @@ func _exit_tree() -> void:
 func _ready() -> void:
 	ap_flask_bar.max_value = PlayerStats.get_max_alchemy_points()
 	PlayerStats.ap_updated.connect(_on_ap_updated)
+	_on_ap_updated()
 	hp_flask_bar.max_value = PlayerStats.max_health
 	PlayerStats.health_updated.connect(_on_hp_updated)
+	_on_hp_updated()
 	reagent_drop_handler.ability_execute_requested.connect(
 		func(ability_name: String) -> void:
 			ability_execute_requested.emit(ability_name)
 	)
 
 func _on_ap_updated() -> void:
-	ap_flask_bar.value = PlayerStats.alchemy_points
+	ap_flask_bar.update_value(PlayerStats.alchemy_points)
 
 func _on_hp_updated() -> void:
-	hp_flask_bar.value = PlayerStats.health
+	hp_flask_bar.update_value(PlayerStats.health)
