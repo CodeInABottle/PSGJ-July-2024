ee8102c772e65867ae34e81c2ffa1bd1ce905099
Jeren Raquel <jeraquel101@gmail.com>
2024-07-23 18:38:54 -0700

    removed swimming

--- regions/RegionBase/player/player_visual_controller.gd
+++ regions/RegionBase/player/player_visual_controller.gd
@@ -1,30 +1,4 @@
 extends Node2D
 
-enum State { LAND, WADE, SWIM }
-
 @onready var player_sprite: AnimatedSprite2D = %PlayerSprite
 @onready var player_pickup_area: Area2D = %PlayerPickupArea
-@onready var animation_player: AnimationPlayer = %AnimationPlayer
-@onready var timer: Timer = %Timer
-
-var _state: State = State.LAND
-
-func _on_water_detector_area_entered(area: Area2D) -> void:
-	if area.get_collision_layer_value(4):	# Shallow Water
-		if _state == State.SWIM:
-			animation_player.play("SwimToWade")
-		else:
-			animation_player.play("Wade")
-		_state = State.WADE
-	if area.get_collision_layer_value(5):	# Swimming Water
-		_state = State.SWIM
-		animation_player.play("Swim")
-
-func _on_water_detector_area_exited(area: Area2D) -> void:
-	if area.get_collision_layer_value(4):	# Shallow Water
-		if animation_player.is_playing(): return
-		timer.start()
-
-func _on_timer_timeout() -> void:
-	_state = State.LAND
-	animation_player.play("RESET")
