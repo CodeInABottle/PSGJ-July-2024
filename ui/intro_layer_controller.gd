81afd30c3089bda28e8b42c6f67a4954999a61d0
Jeren Raquel <jeraquel101@gmail.com>
2024-07-28 10:24:50 -0700

    quick area intro disapear on movement

--- ui/intro_layer_controller.gd
+++ ui/intro_layer_controller.gd
@@ -8,21 +8,34 @@ extends CanvasLayer
 @export var fade_duration: float = 1.0
 @export var total_duration: float = 3.0
 
+var _tween: Tween
+
 func _ready() -> void:
 	map.modulate = Color(1, 1, 1, 0)
 	intro_timer.timeout.connect(_on_intro_timer_timeout)
 	LevelManager.world_event_occurred.connect(on_world_event)
 	DialogueManager.dialogue_started.connect(on_dialogue_started)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("forward") or event.is_action_pressed("backward")\
+			or event.is_action_pressed("left") or event.is_action_pressed("right"):
+		if _tween: _tween.kill()
+		_tween = map.create_tween()
+		_tween.tween_property(map, "modulate", Color(1,1,1,0), 0.25)
+
 func _on_intro_timer_timeout() -> void:
-	var name_tween: Tween = map.create_tween()
-	name_tween.tween_property(map, "modulate", Color(1,1,1,0), fade_duration)
+	if _tween:
+		_tween.kill()
+	_tween = map.create_tween()
+	_tween.tween_property(map, "modulate", Color(1,1,1,0), fade_duration)
 
 func trigger() -> void:
 	name_label.text = LevelManager.region_name
 
-	var name_tween: Tween = map.create_tween()
-	name_tween.tween_property(map, "modulate", Color(1,1,1,1), fade_duration)
+	if _tween:
+		_tween.kill()
+	_tween = map.create_tween()
+	_tween.tween_property(map, "modulate", Color(1,1,1,1), fade_duration)
 	intro_timer.start(total_duration)
 
 func on_world_event(event_name: String, _args:Array) -> void:
